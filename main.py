20de3063fab1b464e8bdf82edbb91af670b20cf5
Jack Grady <275314523+mudskips@users.noreply.github.com>
2026-08-08 09:40:48 -0400

    remove uncalled test function

--- main.py
+++ main.py
@@ -87,18 +87,6 @@ def make_predictions(X, W1, b1, W2, b2):
     predictions = get_predictions(A2)
     return predictions
 
-def test_prediction(index, W1, b1, W2, b2):
-    current_image = X_train[:, index, None]
-    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
-    label = Y_train[index]
-    print("Prediction: ", prediction)
-    print("Label: ", label)
-    
-    current_image = current_image.reshape((28, 28)) * 255
-    plt.gray()
-    plt.imshow(current_image, interpolation='nearest')
-    plt.show()
-
 dev_predictions = make_predictions(X_dev, W1, b1, W2, b2)
 print(f"Dev accuracy: {get_accuracy(dev_predictions, Y_dev)}")
 
